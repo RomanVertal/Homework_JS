3fd1713d9032a5e0b3f93d129198c61af692febf
RomanVertal <rockerzlolz@gmail.com>
2023-05-19 23:24:44 +0300

    add movie form + adaptive

--- src/index.js
+++ src/index.js
@@ -7,4 +7,15 @@ asyncFunc()
 
 
 
+const addMovieForm = document.querySelector('.add_movie')
+const addMovieButton =document.querySelector('.button_add_movie')
+const closeMovieButton =document.querySelector('.add_movie__form-button-close')
+
+const toggleAddMovieForm = (() => {
+    addMovieForm.classList.toggle('hidden_block')
+})
+
+addMovieButton.addEventListener('click' , toggleAddMovieForm)
+closeMovieButton.addEventListener('click' , toggleAddMovieForm)
+
 
